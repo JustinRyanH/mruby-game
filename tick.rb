ac4a82bb331b900c6f992a81b4e3246fd6e0bd60
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-04-18 18:33:40 -0500

    feat: user ruby adder/setter

--- tick.rb
+++ tick.rb
@@ -6,7 +6,7 @@ input = FrameInput.new
 
 amount = 100 * input.delta_time
 
-player.x = player.x + amount if input.key_down?(:d)
-player.x = player.x - amount if input.key_down?(:a)
-player.y = player.y - amount if input.key_down?(:w)
-player.y = player.y + amount if input.key_down?(:s)
+player.x += amount if input.key_down?(:d)
+player.x -= amount if input.key_down?(:a)
+player.y -= amount if input.key_down?(:w)
+player.y += amount if input.key_down?(:s)
