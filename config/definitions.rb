834188c0be9f7ff06287aed801c83b604c7ccad9
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-04-21 16:27:10 -0500

    document log and vector

--- config/definitions.rb
+++ config/definitions.rb
@@ -1,7 +1,48 @@
 # frozen_string_literal: true
 
+class Log
+  # @param [String] msg
+  def self.info(msg); end
+  # @param [String] msg
+  def self.error(msg); end
+  # @param [String] msg
+  def self.fatal(msg); end
+  # @param [String] msg
+  def self.warning(msg); end
+end
 
-class InputFrame
+class FrameInput
   # @return [Float]
   def self.delta_time; end
+end
+
+class Vector
+  # @param [Float]
+  # @return [Float]
+  attr_accessor :x
+  # @param [Float]
+  # @return [Float]
+  attr_accessor :y
+
+
+  # Create a Zero Vector
+  #
+  # @return [Vector]
+  def self.zero; end
+
+  # @param [Float] x
+  # @param [Float] y
+  def initialize(x, y); end
+
+  # Adds two vectors together
+  #
+  # @param [Vector] other
+  # @return [Vector] new_value
+  def +(other); end
+
+  # Scales a Vector
+  #
+  # @param [Float] scalar
+  # @return [Vector]
+  def *(other); end
 end
