9b3de8d81a1481fccec46772d68f9fa8f6d9e3bf
Justin Hurstwright <jryanhurst@gmail.com>
2024-04-22 22:27:56 -0500

    remove the debug lines

--- rb/tick.rb
+++ rb/tick.rb
@@ -5,11 +5,3 @@
 # puts h
 game = Game.current
 game.tick
-
-text = 'Really, Really Long Text'
-
-ImUI.draw_rect(left: 50, right: 450, top: 50, bottom: 140, color: Color.orange)
-ImUI.draw_text(text:, pos: Vector.new(60, 50 + 12), size: 24, color: Color.black, alignment: :left)
-ImUI.draw_text(text:, pos: Vector.new(450 - 12, 50 + 36), size: 24, color: Color.black, alignment: :right)
-ImUI.draw_text(text:, pos: Vector.new(50 + ((450 - 50) * 0.5), 50 + 36 + 24), size: 24, color: Color.black,
-               alignment: :center)
