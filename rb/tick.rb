14738b1aa1786b83b5bfcd7ef4a0636b293f412e
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-04-23 08:47:31 -0500

    ref: remove the items

--- rb/tick.rb
+++ rb/tick.rb
@@ -5,13 +5,3 @@
 # puts h
 game = Game.current
 game.tick
-
-y = (Math.sin(FrameInput.id * 0.01) + 1) * 0.5
-x = (Math.cos(FrameInput.id * 0.01) + 1) * 0.5
-
-ImUI.draw_rect(
-  pos: Vector.new(400, 100),
-  size: Vector.new(75, 100),
-  anchor_percentage: Vector.new(x, y),
-  mode: :outline
-)
