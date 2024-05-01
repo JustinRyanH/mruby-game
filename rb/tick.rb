65b5de44b6a07e4d32efe187faf9186ce17cfd97
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-05-01 18:21:11 -0500

    feat: re-implement game

--- rb/tick.rb
+++ rb/tick.rb
@@ -1,18 +1,9 @@
 # frozen_string_literal: true
 
-require 'assets/scripts/assets'
+require 'assets/scripts/setup'
 # h = {}
 # ObjectSpace.count_objects(h)
 # puts h
 
-$spr ||= Sprite.create(texture: Textures.copter, size: Vector.new(64, 64)).tap do |spr|
-  spr.tint = Color.red
-  spr.pos = Vector.new(400, 600)
-end
-# $spr.texture = (FrameInput.id % 2).zero? ? Textures.copter : Textures.copter3
-
-$spr.visible = !$spr.visible? if FrameInput.key_just_pressed?(:t)
-$spr.destroy if FrameInput.key_just_pressed?(:k)
-
-# $game ||= Game.new
-# $game.tick
+$game ||= Game.new
+$game.tick
