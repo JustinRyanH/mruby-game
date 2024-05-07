2fd137551c00cde1f601bca0ec87031dc201240b
Justin Hurstwright <jryanhurst@gmail.com>
2024-05-07 05:50:17 -0500

    feat: Basic IMGUI

--- rb/tick.rb
+++ rb/tick.rb
@@ -7,15 +7,14 @@
 
 # $game ||= Game.new
 # $game.tick
-#
+require 'assets/scripts/engine_override'
 require 'assets/scripts/assets'
 
 class ImUiText
   attr_reader :message, :pos, :size, :font
 
-  def initialize(message, pos:)
+  def initialize(message)
     @message = message
-    @pos = pos
     @size = 32
     @font = Fonts.kenney_future
   end
@@ -39,27 +38,29 @@ class ImUiContainer
   end
 
   def text(message)
-    @elements << ImUiText.new(message, pos:)
+    @elements << ImUiText.new(message)
   end
 
   def draw
-    new_size = Vector.zero
-    @elements.each do |el|
-      dims = el.dimensions
-      new_size.y += dims.y
-      new_size.y += @padding
-      new_size.x = [new_size.x, dims.x].max
-    end
-    new_size.x += @padding
+    dimensions = @elements.map(&:dimensions)
+    height = dimensions.inject(0) { |sum, dim| sum + dim.y + @padding } + @padding
+    width = dimensions.inject(0) { |max, dim| [max, dim.x].max } + (@padding * 2)
+
+    calculated_size = Vector.new(width, height)
+    rect = Rectangle.new(pos:, size: calculated_size)
 
     Draw.rect(
       pos:,
-      size: new_size,
+      size: calculated_size,
       color: Color.regal_blue,
       anchor_percentage: Vector.new(0.5, 0.5),
     )
+    y = rect.top + @padding
     @elements.each do |el|
-      Draw.text(text: el.message, pos: el.pos, font: el.font, size: el.size, halign: :center)
+      dimensions = el.dimensions
+      pos = Vector.new(@pos.x, (dimensions.y * 0.5) + y)
+      y += @padding + dimensions.y
+      Draw.text(text: el.message, pos:, font: el.font, size: el.size, halign: :center)
     end
   end
 
@@ -90,6 +91,7 @@ class Demo
     Engine.background_color = Color.crow_black_blue
 
     ImUI.container do |ui|
+      ui.text('Immediate Mode GUI')
       ui.text('Immediate Mode GUI')
     end
   end
