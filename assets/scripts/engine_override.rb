21fa97562735c8b121d5643ee2458d695ed7e4ea
Justin Hurstwright <jryanhurst@gmail.com>
2024-05-13 17:54:25 -0500

    ref: Move method to own file

--- assets/scripts/engine_override.rb
+++ assets/scripts/engine_override.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
 
-module Bounds
-  def bottom
-    pos.y + (size.y * 0.5)
-  end
-
-  def top
-    pos.y - (size.y * 0.5)
-  end
-
-  def left
-    pos.x - (size.x * 0.5)
-  end
-
-  def right
-    pos.x + (size.x * 0.5)
-  end
-end
+require 'assets/scripts/common'
 
 Integer.class_eval do
   def even?
