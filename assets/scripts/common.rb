dd594240532a7b3f3e08843fe032189bce9aa518
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-05-16 14:19:18 -0500

    feat: Implement basic field access module

--- assets/scripts/common.rb
+++ assets/scripts/common.rb
@@ -17,3 +17,39 @@ module Bounds
     pos.x + (size.x * 0.5)
   end
 end
+
+module FieldAccessor
+  def self.included(klass)
+    klass.extend(ClassMethods)
+    klass.include(InstanceMethods)
+  end
+
+  module InstanceMethods
+    attr_reader :attributes
+
+    def initialize(**kwargs)
+      @attributes = kwargs.to_h
+    end
+
+    def to_h
+      self.class.all_fields.each_with_object({}) do |field, out|
+        out[field] = send(field)
+      end
+    end
+  end
+
+  module ClassMethods
+    def field(method_name, default: nil)
+      all_fields << method_name
+      create_getter(method_name, default)
+    end
+
+    def all_fields
+      @all_fields ||= []
+    end
+
+    def create_getter(method_name, default_value)
+      define_method(method_name) { attributes[method_name] || default_value }
+    end
+  end
+end
