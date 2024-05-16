64cca44253f0c0aeca0e77abe8e7cace05bd2ffe
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-05-15 19:52:31 -0500

    ref: remove demo code

--- rb/tick.rb
+++ rb/tick.rb
@@ -57,21 +57,6 @@ class Demo
         ui.focus_element.pos.x = ui.left - 32 if btn.focused?
         puts 'Show High Score' if btn.clicked?
       end
-      ui.button('A 1', style: button_style, hover_style:, down_style:) do |btn|
-        ui.focus_element.pos.y = btn.pos.y if btn.focused?
-        ui.focus_element.pos.x = ui.left - 32 if btn.focused?
-        puts 'Show High Score' if btn.clicked?
-      end
-      ui.button('A 2', style: button_style, hover_style:, down_style:) do |btn|
-        ui.focus_element.pos.y = btn.pos.y if btn.focused?
-        ui.focus_element.pos.x = ui.left - 32 if btn.focused?
-        puts 'Show High Score' if btn.clicked?
-      end
-      ui.button('A 3', style: button_style, hover_style:, down_style:) do |btn|
-        ui.focus_element.pos.y = btn.pos.y if btn.focused?
-        ui.focus_element.pos.x = ui.left - 32 if btn.focused?
-        puts 'Show High Score' if btn.clicked?
-      end
       ui.button('Exit', style: button_style, hover_style:, down_style:) do |btn|
         ui.focus_element.pos.y = btn.pos.y if btn.focused?
         ui.focus_element.pos.x = ui.left - 32 if btn.focused?
