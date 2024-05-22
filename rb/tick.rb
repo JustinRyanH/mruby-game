36754af6a73c5b7a4fbaafd626c131d350883bcf
Justin Hurstwright <jryanhurst@gmail.com>
2024-05-22 14:48:46 -0500

    spike: setup demo scene

--- rb/tick.rb
+++ rb/tick.rb
@@ -5,5 +5,22 @@ require 'assets/scripts/setup'
 # ObjectSpace.count_objects(h)
 # puts h
 
-$game ||= Game.new
-$game.tick
+# $game ||= Game.new
+# $game.tick
+
+class SpringEpxeriment
+  def tick
+    width, height = FrameInput.screen_size
+    Draw.text(text: 'Spring Example', size: 64, pos: Vector.new(width / 2, 70), color: Color.red, halign: :center)
+
+    @spring_pos ||= Vector.new(width / 2, height / 2)
+    @spring_min ||= 100
+    @spring_max ||= width - 100
+
+    Draw.rect(pos: @spring_pos, size: Vector.new(@spring_max - @spring_min, 8), color: Color.white)
+    Draw.rect(pos: @spring_pos, size: Vector.new(75, 75))
+  end
+end
+
+$spring ||= SpringEpxeriment.new
+$spring.tick
