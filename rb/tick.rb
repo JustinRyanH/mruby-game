d8ce230d8a81508da98fb1b1989bd8c347e1f6b4
Justin Ryan Hurstwright <jryanhurst@gmail.com>
2024-05-29 19:51:14 -0500

    feat: Get world space from screen space

--- rb/tick.rb
+++ rb/tick.rb
@@ -62,8 +62,20 @@ class TestGame
     setup unless @started
     @player.tick
 
+    @camera.spring.frequency = 6
+    @camera.spring.damping = 0.8
     @camera.pos.x = @player.pos.x
     @camera.tick
+
+    mouse_pos = FrameInput.mouse_pos
+    world_space = Camera.current.screen_to_world(mouse_pos)
+
+    screen_size = FrameInput.screen.size
+    text_pos = Vector.new(screen_size.x * 0.5, screen_size.y - 80)
+    Draw.text(text: "Mouse ScreenSpace: (x: #{mouse_pos.x.round(2)}, y: #{mouse_pos.y.round(2)})",
+              pos: text_pos)
+    Draw.text(text: "Mouse WorldSpace: (x: #{world_space.x.round(2)}, y: #{world_space.y.round(2)})",
+              pos: text_pos + Vector.new(0, 30))
   end
 
   def setup
@@ -73,7 +85,6 @@ class TestGame
     Sprite.create(pos: Vector.zero, size: bg_size, texture: Textures.bg0)
 
     screen = FrameInput.screen
-    puts "screen.left: #{screen.left}, screen.right: #{screen.right}, screen.top: #{screen.top}, screen.bottom: #{screen.bottom}"
 
     create_camera
     create_player
