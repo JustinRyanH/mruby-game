f1918d8fa3464a6592cc527734193b80b41c6a29
Justin Hurstwright <jryanhurst@gmail.com>
2024-06-16 20:02:33 -0500

    feat: start texture packing

--- rb/tick.rb
+++ rb/tick.rb
@@ -9,8 +9,25 @@
 # $old_game ||= Game.new
 # $old_game.tick
 
-require 'assets/scripts/setup'
-require 'assets/scripts/new_game'
+# require 'assets/scripts/setup'
+# require 'assets/scripts/new_game'
+#
+# $new_game ||= TestGame.new
+# $new_game.tick
 
-$new_game ||= TestGame.new
-$new_game.tick
+class TexturePacking
+  def tick
+    setup unless ready?
+  end
+
+  def setup
+    @ready = true
+  end
+
+  def ready?
+    @ready || false
+  end
+end
+
+$packing ||= TexturePacking.new
+$packing.tick
