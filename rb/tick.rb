6ae9ac4ec3cb5cc35803728760d5b4fb6fa1f7e0
Justin Hurstwright <jryanhurst@gmail.com>
2024-06-28 22:42:47 -0500

    feat: setup sonar game pipeline

--- rb/tick.rb
+++ rb/tick.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
-require 'assets/scripts/setup'
-require 'assets/scripts/engine_override'
+# require 'assets/scripts/setup'
+# require 'assets/scripts/engine_override'
 # h = {}
 # ObjectSpace.count_objects(h)
 # pugts h
 
-$old_game ||= Game.new
-$old_game.tick
+# $old_game ||= Game.new
+# $old_game.tick
+#
+
+class RevealGame
+  def tick
+    setup unless ready?
+    screen = FrameInput.screen
+
+    text_pos = Vector.new(screen.size.x * 0.5, screen.size.y - 72)
+    Draw.text(text: 'Sonar Test', pos: text_pos, halign: :center, size: 60)
+  end
+
+  private
+
+  def ready?
+    @ready || false
+  end
+
+  def setup
+    @ready = true
+  end
+end
+
+$game ||= RevealGame.new
+$game.tick
